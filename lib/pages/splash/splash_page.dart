de82da5c35b4e27486d80be2410fb6e1e172381a
mamunhossain <mh72156@gmail.com>
2024-03-25 00:36:47 +0600

    Fix splash screen image issue

--- lib/pages/splash/splash_page.dart
+++ lib/pages/splash/splash_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'bloc/splash_bloc.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -10,8 +12,13 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Image.asset("assets/images/splash.png"),
+    return BlocProvider(
+      create: (context) => SplashBloc(context: context),
+      child: Scaffold(
+        body: Center(
+          child: Image.asset("assets/images/splash.png",scale: 3,),
+        ),
+      ),
     );
   }
 }
